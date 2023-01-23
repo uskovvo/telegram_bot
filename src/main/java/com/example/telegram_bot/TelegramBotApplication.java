ce3381778e7b8bbf0f4d9522cbf48aef4e8e6e4c
valerii <uskoovvospb@gmail.com>
2023-01-23 22:04:01 +0100

    change packaging

--- src/main/java/com/example/telegram_bot/TelegramBotApplication.java
+++ src/main/java/com/example/telegram_bot/TelegramBotApplication.java
@@ -2,12 +2,25 @@ package com.example.telegram_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class TelegramBotApplication {
+public class TelegramBotApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TelegramBotApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Telegram Bot запущен";
     }
 
 }
