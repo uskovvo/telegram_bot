6447e9fc6751811bcba09c59f02a7e3ce8eb2699
valera <uskovvospb@gmail.com>
2023-04-13 21:38:00 +0200

    Added to OrderEntity Timestamp and changed logback.xml

--- src/main/java/com/example/telegram_bot/model/OrderEntity.java
+++ src/main/java/com/example/telegram_bot/model/OrderEntity.java
@@ -1,13 +1,16 @@
 package com.example.telegram_bot.model;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
-@Entity
+@Entity(name = "orderData")
 public class OrderEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Timestamp dateOrderWasAdded;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -19,6 +22,14 @@ public class OrderEntity {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Timestamp getDateOrderWasAdded() {
+        return dateOrderWasAdded;
+    }
+
+    public void setDateOrderWasAdded(Timestamp dateOrderWasAdded) {
+        this.dateOrderWasAdded = dateOrderWasAdded;
     }
 
     public UserEntity getUserEntity() {
