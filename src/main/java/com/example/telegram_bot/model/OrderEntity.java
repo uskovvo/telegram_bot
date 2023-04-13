9090e76a2cf1840f5bef5c81511d76d464b7726c
valera <uskovvospb@gmail.com>
2023-04-13 20:13:11 +0200

    UserEntity is changed

--- src/main/java/com/example/telegram_bot/model/OrderEntity.java
+++ src/main/java/com/example/telegram_bot/model/OrderEntity.java
@@ -1,4 +1,15 @@
 package com.example.telegram_bot.model;
 
+import javax.persistence.*;
+
+@Entity
 public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
 }
--- src/main/java/com/example/telegram_bot/model/UserEntity.java
+++ src/main/java/com/example/telegram_bot/model/UserEntity.java
@@ -1,13 +1,13 @@
 package com.example.telegram_bot.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Entity(name = "usersData")
-public class User {
+public class UserEntity {
 
     @Id
     private Long chatId;
@@ -15,6 +15,9 @@ public class User {
     private String lastName;
     private String userName;
     private Timestamp registeredAt;
+
+    @OneToMany(mappedBy = "userEntity")
+    private List<OrderEntity> orders;
 
     public Long getChatId() {
         return chatId;
